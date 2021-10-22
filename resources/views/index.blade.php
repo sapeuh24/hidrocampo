f8b6fa3f6e157351637266de7b8d087bb39d68b9
Oscar Arias <eduardo93-12@hotmail.com>
2021-10-22 16:38:46 -0500

    Ajustes Oscar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,24 +11,18 @@
     <div class="carousel-item active">
     <img src="{{ asset('img/banner1.jpg') }}" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <h5>SISTEMA <br><span>DE RIEGO</span></h5>
+        <p>Es un componente tecnologico basico, programable para optimizar el riego de los cultivos .</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="{{ asset('img/banner2.jpg') }}" class="d-block w-100" alt="...">
+      <img src="{{ asset('img/banner3.jpg') }}" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
+
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
